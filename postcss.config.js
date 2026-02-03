908706c1a7f8e960c4608663f96fe69ad8077c3b
rainathecreatour-ops <rainathecreatour@gmail.com>
2026-02-03 07:50:39 -0500

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+---
+
+### **File 9: `.gitignore`**
+```
+# Dependencies
+node_modules/
+
+# Build output
+dist/
+
+# Environment variables
+.env
+.env.local
+
+# IDE
+.vscode/
+.idea/
+
+# OS
+.DS_Store
+Thumbs.db
